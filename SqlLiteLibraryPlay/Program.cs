b987adb1f61a8e095580a7ca6db1362c4d5052ae
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-06-04 00:42:35 -0500

    added in my book and bookshelf class

--- SqlLiteLibraryPlay/Program.cs
+++ SqlLiteLibraryPlay/Program.cs
@@ -9,4 +9,31 @@ namespace SqlLiteLibraryPlay
             Console.WriteLine("Hello World!");
         }
     }
+
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        public Book(int id, string title, string genre)
+        {
+            id = ID;
+            Title = title;
+            Genre = genre;
+        }
+    }
+
+    public class BookShelf
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Book[] books { get; set; }
+
+        public BookShelf(int id, string name)
+        {
+            id = ID;
+            name = Name;
+        }
+    }
 }
