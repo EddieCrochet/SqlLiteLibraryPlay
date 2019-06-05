05b79bd89444b7a00eddcb9e577b7f8ac38601c9
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-06-05 18:15:48 -0500

    Had to update my book model by adding the author property so this is the migration of data that took place for that to come through.

--- SqlLiteLibraryPlay/Migrations/20190605231433_AuthorToBook.Designer.cs
+++ SqlLiteLibraryPlay/Migrations/20190605231433_AuthorToBook.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SqlLiteLibraryPlay;
 
 namespace SqlLiteLibraryPlay.Migrations
 {
     [DbContext(typeof(SqlLiteLibraryPlayContext))]
-    partial class SqlLiteLibraryPlayContextModelSnapshot : ModelSnapshot
+    [Migration("20190605231433_AuthorToBook")]
+    partial class AuthorToBook
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SqlLiteLibraryPlay/Migrations/20190605231433_AuthorToBook.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SqlLiteLibraryPlay.Migrations
+{
+    public partial class AuthorToBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "books");
+        }
+    }
+}
