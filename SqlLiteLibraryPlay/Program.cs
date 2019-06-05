aa8b4a05e290fec2e60cb00f1727b847be69f6cc
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-06-05 18:23:41 -0500

    instantiated more books - enough to fir on my bookshelf I instantiated

--- SqlLiteLibraryPlay/Program.cs
+++ SqlLiteLibraryPlay/Program.cs
@@ -9,7 +9,19 @@ namespace SqlLiteLibraryPlay
     {
         static void Main(string[] args)
         {
+            BookShelf BottomLeft = new BookShelf("BottomLeft", new Book[8]);
+            //Bottom left shelfonly holds 8 books
+
             Book b = new Book("The Foos and The Hound", "Rob Stanley", "Horror");
+            Book b1 = new Book("blergh", "jum k", "fiction");
+            Book b2 = new Book("grey", "Rob hun", "fact");
+            Book b3 = new Book("jill waked one mile", "peggy Stanley", "Horror");
+            Book b4 = new Book("6 pills", "Rob Stanley", "Thriller");
+            Book b5 = new Book("Joey went back", "Joey Back", "Boigaphy");
+            Book b6 = new Book("Hal About it", "Hal hallerson", "Comedy");
+            Book b7 = new Book("TDave", "Theodore David", "Biography");
+            Book b8 = new Book("Alphabet", "Kim Who", "Childrens");
+ 
         }
     }
 
@@ -45,9 +57,10 @@ namespace SqlLiteLibraryPlay
         public string Name { get; set; }
         public Book[] books { get; set; }
 
-        public BookShelf(string name)
+        public BookShelf(string name, Book[] _books)
         {
             name = Name;
+
         }
     }
 }
